25e060c0b9552504d7c35aad466e8de780f06e79
DimitriTedom <dimitritedom@gmail.com>
2025-04-18 02:30:28 +0100

    deployement

--- Server/dist/routes/shop/search.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const Search_controller_1 = require("../../controllers/shop/Search.controller");
-const router = (0, express_1.Router)();
-router.get('/:keyword', Search_controller_1.SearchProducts);
-exports.default = router;
-//# sourceMappingURL=search.routes.js.map
